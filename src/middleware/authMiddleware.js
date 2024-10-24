38c00e77492d8e22a91c761b9497a2686173459e
Samarth Shinde <samarthsshinde@gmail.com>
2024-10-24 15:26:16 +0530

    auth middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+	let token;
+	let authHeader = req.headers.Authorization || req.headers.authorization;
+
+	if (authHeader && authHeader.startsWith("Bearer")) {
+		token = authHeader.split(" ")[1];
+	}
+
+	if (token) {
+		try {
+			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+			req.user = decoded;
+			next();
+		} catch (error) {
+			return res.status(400).json({ message: "Token is not valid" });
+		}
+	} else {
+		return res.status(400).json({ message: "No token, Not authorized" });
+	}
+};
+
+export default verifyToken;
