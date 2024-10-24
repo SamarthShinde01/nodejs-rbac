f203a4e383097f7e207789fb4ffda9c456977af9
Samarth Shinde <samarthsshinde@gmail.com>
2024-10-24 15:11:31 +0530

    authRotuer

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,7 +1,50 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js";
+
 const register = async (req, res) => {
-	const { username, password, role } = req.body;
+	try {
+		const { username, password, role } = req.body;
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const newUser = await User({ username, password: hashedPassword, role });
+		await newUser.save();
+
+		return res
+			.status(200)
+			.json({ message: "User registerd succesfully", newUser });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: error.message });
+	}
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+
+		const user = await User.findOne({ username });
+		if (!user) {
+			return res.status(400).json({ message: "User not found" });
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+
+		if (!isMatch) {
+			return res.status(400).json({ message: "Invalid Credentials" });
+		}
+
+		const token = jwt.sign(
+			{ userId: user._id, role: user.role },
+			process.env.JWT_SECRET,
+			{ expiresIn: "2hr" }
+		);
+
+		return res.status(200).json({ token });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: error.message });
+	}
+};
 
 export { register, login };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoutes.js";
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
-connectDB();
+
+app.use("/api/auth", authRoute);
+
 app.get("/", (req, res) => {
 	return res.json("server is running...");
 });
